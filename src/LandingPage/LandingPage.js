53a89640f658e0406fb218534f9d0b9f03bbf2fd
kvphan5 <kvphan5@gmail.com>
2021-08-20 11:24:59 -0700

    add logo

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { TopNav } from './TopNav/TopNav';
+import logo from '../assets/logo192.png';
+import styles from './LandingPage.module.css';
 
 export function LandingPage(){
     return(
         <div>
             <TopNav />
+            <img src={logo} alt="Logo" className={styles.logo}/>
         </div>
     );
 }
